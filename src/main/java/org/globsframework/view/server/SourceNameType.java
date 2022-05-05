aadc203455e2008db00fe5c90962f3121f365176
MarcGuiot <marc.guiot@free.fr>
2022-05-05 12:42:05 +0200

    create on SourceNameType

--- src/main/java/org/globsframework/view/server/SourceNameType.java
+++ src/main/java/org/globsframework/view/server/SourceNameType.java
@@ -5,6 +5,7 @@ import org.globsframework.metamodel.GlobTypeLoaderFactory;
 import org.globsframework.metamodel.annotations.KeyField;
 import org.globsframework.metamodel.fields.StringArrayField;
 import org.globsframework.metamodel.fields.StringField;
+import org.globsframework.model.Glob;
 
 public class SourceNameType {
     public static GlobType TYPE;
@@ -13,6 +14,12 @@ public class SourceNameType {
     public static StringField ID;
 
     public static StringArrayField NAME;
+
+    public static Glob create(String id, String... name) {
+        return TYPE.instantiate()
+                .set(ID, id)
+                .set(NAME, name);
+    }
 
     static {
         GlobTypeLoaderFactory.create(SourceNameType.class).load();
